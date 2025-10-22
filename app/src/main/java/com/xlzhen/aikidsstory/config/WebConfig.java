9a378755398ca7698cd169fca22c4f40ccc85e95
xlzhen <xionglongzhen@gmail.com>
2025-10-22 16:40:06 +0800

    忽略该文件

--- app/src/main/java/com/xlzhen/aikidsstory/config/WebConfig.java
+++ app/src/main/java/com/xlzhen/aikidsstory/config/WebConfig.java
@@ -1,5 +1,5 @@
 package com.xlzhen.aikidsstory.config;
 
 public class WebConfig {
-    public static final String SERVER_URL = "http://127.0.0.1:29454/generate_story";
+    public static final String SERVER_URL = "http://107.175.50.107:29454/generate_story";
 }
